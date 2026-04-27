77a5d7ef041061fb8ebc60b59b6c78b30d86f721
Surajbasheer <surajbasheer21@gmail.com>
2026-04-27 15:11:03 +0530

    Add HelloWorld Java application class

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from Simple Java Application!");
+        System.out.println("Ready for Jenkins deployment!");
+    }
+    
+    public static String getGreeting() {
+        return "Welcome to CI/CD with Jenkins";
+    }
+}
